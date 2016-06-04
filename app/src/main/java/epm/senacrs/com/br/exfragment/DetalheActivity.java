c3861bb10d08ea209dab17fe45982306412bfc9f
Gaspar Teixeira <gaspar.teixeira@gmail.com>
2016-06-04 15:37:26 -0300

    adicionadondo acoes aos fragments

--- app/src/main/java/epm/senacrs/com/br/exfragment/DetalheActivity.java
+++ app/src/main/java/epm/senacrs/com/br/exfragment/DetalheActivity.java
@@ -1,5 +1,7 @@
 package epm.senacrs.com.br.exfragment;
 
+import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,15 @@ public class DetalheActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+        }
         setContentView(R.layout.activity_detalhe);
+
+        String time = getIntent().getStringExtra("time");
+
+        FragmentManager fragmentManager = getFragmentManager();
+        DetalheFrag detalheFrag = (DetalheFrag)fragmentManager.findFragmentById(R.id.detalhe_frag);
+        detalheFrag.setNomes(time);
     }
 }
--- app/src/main/java/epm/senacrs/com/br/exfragment/MainActivity.java
+++ app/src/main/java/epm/senacrs/com/br/exfragment/MainActivity.java
@@ -1,10 +1,13 @@
 package epm.senacrs.com.br.exfragment;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements CliqueLista{
+
+    private String timeBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,14 +16,26 @@ public class MainActivity extends AppCompatActivity implements CliqueLista{
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        if(timeBack != null) {
+            clicou(timeBack);
+            timeBack = null;
+        }
+    }
+
+    @Override
     public void clicou(String time) {
         FragmentManager fragmentManager = getFragmentManager();
         DetalheFrag detalheFrag = (DetalheFrag)fragmentManager.findFragmentById(R.id.detalheFrag);
 
         if(detalheFrag != null && detalheFrag.isInLayout()) {
-
+            detalheFrag.setNomes(time);
         } else {
-
+            timeBack = time;
+            Intent intent = new Intent(this, DetalheActivity.class);
+            intent.putExtra("time", time);
+            startActivity(intent);
         }
     }
 }
